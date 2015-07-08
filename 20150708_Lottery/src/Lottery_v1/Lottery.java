12de9a08bee055317616e72537686f549044a0ef
Klaus Chou <klaus580925@gmail.com>
2015-07-08 23:30:09 +0800

    20150708_Lottery 開發_20150708002
    1. 開發功能

--- 20150708_Lottery/src/Lottery_v1/Lottery.java
+++ 20150708_Lottery/src/Lottery_v1/Lottery.java
@@ -1,9 +1,47 @@
 package Lottery_v1;
 
+import java.util.ArrayList;
+
 public class Lottery {
-    
-    for (int i = 0; i < 6; i++) {
-            System.out.println((int)(Math.random() * 100));
-}
+
+    private static ArrayList numArray;
+    private static ArrayList lottArray;
+    private static int index;
+
+    public static void randFactory() {
+        for (int i = 0; i < 6; i++) {
+            System.out.println((int) (Math.random() * 42) + 1);          // 生成亂數 1 ~ 42
+        }
+    }
+
+    /**
+     * 建立數字與樂透陣列物件
+     */
+    private static void setupArray() {
+
+        Lottery.numArray = new ArrayList();
+        Lottery.lottArray = new ArrayList();
+
+    }
+
+    /**
+     * 生成42個整數，放進 numArray 可調式陣列
+     */
+    private static void numPullInArray() {
+
+        for (int i = 0; i < 42; i++) {
+            Lottery.numArray.add(i);
+        }
+
+    }
+
+    /**
+     * 用亂數產生一個陣列索引
+     */
+    private static void randomSetIndexArray() {
+        
+        Lottery.index = (int)(Math.random() * Lottery.numArray.size());         // 0 ~ 41
+        
+    }
     
 }
--- 20150708_Lottery/src/Lottery_v1/LotteryTest.java
+++ 20150708_Lottery/src/Lottery_v1/LotteryTest.java
@@ -4,9 +4,7 @@ public class LotteryTest {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 6; i++) {
-            System.out.println((int) (Math.random() * 60) + 1);     // 亂數 1 ~ 60
-        }
+        randFactory
 
     }
 
